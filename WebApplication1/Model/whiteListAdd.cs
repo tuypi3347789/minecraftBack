4c049b3660a0cd02cfe206db8e2e4911385086f0
tuypi3347789 <tuypi3347789@gmail.com>
2020-09-03 03:03:27 +0800

    申請帳號成功/json解析進行中

---
+++ WebApplication1/Model/whiteListAdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Google.Protobuf.Reflection;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Model
+{
+    class whiteListAdd
+    {
+        public static void Main(Guid uuid, string name)
+        {
+            string str = @"D:\server\whitelist.json";
+            string josnString = File.ReadAllText(str, Encoding.Default);
+            JArray jo = JArray.Parse(josnString);
+            jo.Add(new JObject(
+                new JProperty("uuid", uuid),
+                new JProperty("name", name)
+            ));
+        }
+    }
+}
